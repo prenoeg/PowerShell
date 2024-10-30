5d7c7b88f7d318e3ea6fa6ff6e4bc69caa520007
prenoeg <prenoeg@proton.me>
2024-10-30 14:42:11 -0700

    Output HTML content for analysis

--- Web/Save-Video.ps1
+++ Web/Save-Video.ps1
@@ -2,6 +2,11 @@ Function Save-Video  {
     Param($video_url)
     
     Write-Host "Attempting to save $video_url" -ForegroundColor Yellow
+
+    $request = Invoke-WebRequest -Uri $video_url
+    
+    # display html code
+    $request.Content
 }
 
-Save-Video $args
+Save-Video $args[0]
