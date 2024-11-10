5cb28da8c689c89254f9f4093b9d95e32992085b
prenoeg <prenoeg@proton.me>
2024-11-10 00:12:10 -0800

    Added code comments

--- Web/Save-Video.ps1
+++ Web/Save-Video.ps1
@@ -3,7 +3,11 @@ Function Save-Video  {
     
     Write-Host "Attempting to save $video_url" -ForegroundColor Yellow
 
+    # using Invoke-WebRequest instead of curl.exe here
     $response = Invoke-WebRequest -Uri $video_url
+
+    # the general idea is to replace all external utility functions with pure powershell
+    # so curl.exe and yt-dlp.exe will be handled directly via powershell...
     
     # display found links
     $response.Links
